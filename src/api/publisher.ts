65f969baac4daaf79e7892023e972c047e81c6ea
Zhanghanwen <zhanghanwen@buaa.edu.cn>
2025-04-26 23:41:25 +0800

    [feat]zhw: publisherAPI

---
+++ src/api/publisher.ts
@@ -0,0 +1,25 @@
+import http from './request'
+
+export default {
+  //发布审核任务
+  dispatchAnnual(data: any) {
+    return http.post('/create_review_task_with_admin_check/', data)
+  },
+
+  //data是taskId
+  //返回某个task的所有人工审核任务的完成情况，只返回百分比
+  getAllAnnual(data: any) {
+    return http.get(`/get_task_completion_status/${data}`)
+  },
+
+ //返回某个task的所有images和所有人工审核的结果
+  getAnnualDetail(data: any) {
+    return http.get(`/get_task_detail/${data}/`)
+  },
+
+  //返回特定审核员对特定任务的审核结果
+  getReviewerDetail(data: any) {
+    return http.get(`/get_task_reviewer_detail/${data.taskId}/${data.reviewer_id}`)
+  }
+
+}
