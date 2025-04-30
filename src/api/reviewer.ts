eb84b6a755f0643b9bb4520fb08d2abc769f1aa8
wyt <22371351@buaa.edu.cn>
2025-04-30 22:03:10 +0800

    [feat]wyt: review

--- src/api/reviewer.ts
+++ src/api/reviewer.ts
@@ -3,5 +3,8 @@ import http from './request'
 export default {
   submitReview(data: any) {
     return http.post(`/submit_review_result/${data.task_id}/`, data)
-  }
+  },
+  getReviewerTasks(params: any) {
+    return http.get('/get_reviewer_tasks/', { params })
+}
 }
