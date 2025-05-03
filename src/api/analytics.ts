bed61b0d803cf8759e64643a56f27f487823bc6c
wyt <22371351@buaa.edu.cn>
2025-05-04 01:57:28 +0800

    [debug]wyt: ……

--- src/api/analytics.ts
+++ src/api/analytics.ts
@@ -1,7 +1,26 @@
 import http from './request'
 
-export default{
-  getTaskData(){
-    return http.get('/admin_dashboard')
+export default {
+  // 获取图像标签统计
+  getImgTag: (params: { startTime?: string; endTime?: string }) => {
+    return http.get('/dashboard/img_tag/', { params })
+  },
+  getTopPublishers: () => {
+    return http.get('/dashboard/top_publishers/')
+  },
+  getDailyTaskCount: () => {
+    return http.get('/dashboard/daily_task_count/')
+  },
+  getDailyReviewRequestCount: () => {
+    return http.get('/dashboard/daily_review_request_count/')
+  },
+  getDailyCompletedManualReviewCount: () => {
+    return http.get('/dashboard/daily_completed_manual_review_count/')
+  },
+  getDailyActiveUsers: () => {
+    return http.get('/dashboard/daily_active_users/')
+  },
+  getDetectionMethodStats: () => {
+    return http.get('/dashboard/get_sub_method_distribution_by_tag/')
   }
 }
